103401ef88d012a7aebff0c87c7488ffe75ad5ff
Marcello Guimaraes <marcello.gpc@hotmail.com>
2020-08-22 16:02:20 -0300

    implements Conjured class following requirements

---
+++ Conjured.cs
@@ -0,0 +1,19 @@
+﻿namespace csharpcore
+{
+    public class Conjured : Item
+    {
+        public Conjured() : base("Conjured")
+        {
+        }
+
+        public override void UpdateQuality()
+        {
+            if (IsNegativeQuality())
+            {
+                return;
+            }
+
+            Quality -= 2;
+        }
+    }
+}
