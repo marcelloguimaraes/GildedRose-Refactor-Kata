c25277e3a00aa5e83b75937b97b581b8ad417f71
Marcello Guimaraes <marcello.gpc@hotmail.com>
2020-08-22 16:02:36 -0300

    refactor Item class to meet requirements

--- Item.cs
+++ Item.cs
@@ -1,9 +1,48 @@
-﻿namespace csharpcore
+﻿using System.Runtime.Serialization;
+
+namespace csharpcore
 {
     public class Item
     {
-        public string Name { get; set; }
+        protected const int MaxQualityValue = 50;
+
+        public string Name { get; private set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void UpdateQuality()
+        {
+            if (IsNegativeQuality())
+            {
+                return;
+            }
+
+            Quality -= 1;
+
+            if (SellIn < 0)
+            {
+                Quality -= 1;
+            }
+        }
+
+        public bool IsNegativeQuality()
+        {
+            return Quality <= 0;
+        }
+
+        public virtual void UpdateSellIn()
+        {
+            SellIn--;
+        }
+
+        public bool CanIncreaseQuality()
+        {
+            return Quality < MaxQualityValue;
+        }
     }
 }
