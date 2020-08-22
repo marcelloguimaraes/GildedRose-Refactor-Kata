e691c287d45900b1e6097d1155dacde1611d48c1
Marcello Guimaraes <marcello.gpc@hotmail.com>
2020-08-22 16:02:47 -0300

    implements Sulfuras class following requirements

---
+++ Sulfuras.cs
@@ -0,0 +1,13 @@
+﻿namespace csharpcore
+{
+    public class Sulfuras : Item
+    {
+        public Sulfuras() : base("Sulfuras, Hand of Ragnaros")
+        {
+        }
+
+        public override void UpdateQuality() { }
+
+        public override void UpdateSellIn() { }
+    }
+}
