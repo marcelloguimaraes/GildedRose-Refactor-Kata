645952a86c985b81a1f25a570faa1514feea67f6
Marcello Guimaraes <marcello.gpc@hotmail.com>
2020-08-22 16:02:05 -0300

    implements Backstage class following requirements

---
+++ BackStagePasses.cs
@@ -0,0 +1,30 @@
+﻿namespace csharpcore
+{
+    public class BackStagePasses : Item
+    {
+        public BackStagePasses() : base ("Backstage passes to a TAFKAL80ETC concert")
+        {
+        }
+
+        public override void UpdateQuality()
+        {
+            if (!CanIncreaseQuality())
+            {
+                return;
+            }
+
+            if (SellIn < 0)
+            {
+                Quality = 0;
+            }
+            else if (SellIn <= 5)
+            {
+                Quality += 3;
+            }
+            else
+            {
+                Quality += 2;
+            }
+        }
+    }
+}
